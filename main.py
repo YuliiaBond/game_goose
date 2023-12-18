94de3f5c900e740e4ed4f2c638fd5ad7a7f22d49
Yuliia Bondarenko <bondarenko047@gmail.com>
2023-12-19 00:17:37 +0200

    2

--- main.py
+++ main.py
@@ -30,20 +30,22 @@ while playing:
 
     main_display.fill(COLOR_BLACK)
 
-    if player_rect.bottom >= HEIGHT:
-        # player_speed = [1, -1]
+    # if player_rect.bottom >= HEIGHT:
+    #     player_speed = [1, -1]
+
+    # if player_rect.right >= WIDTH:
+    #     player_speed = [-1, -1]
+
+    # if player_rect.top == 0:
+    #     player_speed = [-1, 1]
+
+    # if player_rect.left == 0:
+    #     player_speed = [1, 1]
+
+    if player_rect.bottom >= HEIGHT or player_rect.top < 0:
         player_speed[1] = -player_speed[1]
 
-    if player_rect.right >= WIDTH:
-        # player_speed = [-1, -1]
-        player_speed[0] = -player_speed[0]
-
-    if player_rect.top < 0:
-        # player_speed = [-1, 1]
-        player_speed[1] = -player_speed[1]
-
-    if player_rect.left < 0:
-        # player_speed = [1, 1]
+    if player_rect.right >= WIDTH or player_rect.left < 0:
         player_speed[0] = -player_speed[0]
 
     main_display.blit(player, player_rect)
