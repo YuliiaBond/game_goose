8041bfebddde5bd3f36ba2103cabeb776e7a7a46
Yuliia Bondarenko <bondarenko047@gmail.com>
2023-12-19 00:08:53 +0200

    1

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pygame.constants import QUIT
 pygame.init()
 
 FPS = pygame.time.Clock()
+
 HEIGHT = 800
 WIDTH = 1200
 
@@ -37,11 +38,11 @@ while playing:
         # player_speed = [-1, -1]
         player_speed[0] = -player_speed[0]
 
-    if player_rect.top <= 0:
+    if player_rect.top < 0:
         # player_speed = [-1, 1]
         player_speed[1] = -player_speed[1]
 
-    if player_rect.left <= 0:
+    if player_rect.left < 0:
         # player_speed = [1, 1]
         player_speed[0] = -player_speed[0]
 
