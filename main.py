a0b49940b621bc8101aa59c130a2e648858cef32
Yuliia Bondarenko <bondarenko047@gmail.com>
2023-12-20 22:02:58 +0200

    img

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ FPS = pygame.time.Clock()
 HEIGHT = 700
 WIDTH = 1200
 
+FONT = pygame.font.SysFont('Verdana', 40)
+
 COLOR_WHITE = (255, 255, 255)
 COLOR_BLACK = (0, 0, 0)
 COLOR_BLUE = (0, 0, 255)
@@ -50,11 +52,12 @@ def create_bonus():
     return [bonus, bonus_rect, bonus_move]
 
 
-CREATE_BONUS = pygame.USEREVENT + 1
+CREATE_BONUS = pygame.USEREVENT + 2
 pygame.time.set_timer(CREATE_BONUS, 2000)
 
 bonuses = []
 
+score = 0
 
 playing = True
 
@@ -89,10 +92,19 @@ while playing:
         enemy[1] = enemy[1].move(enemy[2])
         main_display.blit(enemy[0], enemy[1])
 
+        if player_rect.colliderect(enemy[1]):
+            playing = False
+
     for bonus in bonuses:
         bonus[1] = bonus[1].move(bonus[2])
         main_display.blit(bonus[0], bonus[1])
 
+        if player_rect.colliderect(bonus[1]):
+            score += 1
+            bonuses.pop(bonuses.index(bonus))
+
+    main_display.blit(FONT.render(str(score), True,
+                      COLOR_WHITE), (WIDTH-50, 20))
     main_display.blit(player, player_rect)
 
     pygame.display.flip()
